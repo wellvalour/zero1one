b7860ebf6b258507eb48b07354d5f1baf5b18cdb
“Til <wohlmuth.til@gmail.com>
2020-09-14 11:37:49 +0200

    Maven-Projekt mit Spring-Framework angelegt

---
+++ zero1one/src/main/java/bdgjmrsww/zero1one/Zero1oneApplication.java
@@ -0,0 +1,13 @@
+package bdgjmrsww.zero1one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zero1oneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zero1oneApplication.class, args);
+	}
+
+}
---
+++ zero1one/src/test/java/bdgjmrsww/zero1one/Zero1oneApplicationTests.java
@@ -0,0 +1,13 @@
+package bdgjmrsww.zero1one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zero1oneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
