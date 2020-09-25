b845471196b9367de852997b851985393bbcf3ca
“Til <wohlmuth.til@gmail.com>
2020-09-25 16:10:32 +0200

    Datenbank überarbeitet

--- site/Datenbank/Datenbankaufsetzung.sql
+++ site/Datenbank/Datenbankaufsetzung.sql
@@ -5,27 +5,32 @@ ID int(5),
 Bezeichnung char(30));
 
 create table User(
-ID int(5),
+-- ID int(5),
 Name char(30),
 Passwort char(30),
-BerechtigungsID int(5));
+BerechtigungsID int(5),
+PRIMARY KEY());
 
 create table ConfigItemTyp(
-ID int (5),
-Name char(30));
+-- ID int (5),
+Name char(30),
+PRIMARY KEY(Name);
 
 create table Instanz(
 ID int(5),
 ConfigItemTypname char (30),
-Name char(30));
+Name char(30),
+PRIMARY KEY(ID));
 
 create table Attributtyp(
-ID int(5),
+-- ID int(5),
 Name char(30),
-ConfigItemTypname char (30));
+ConfigItemTypname char (30),
+PRIMARY KEY(Name));
 
 create table Attribut(
-ID int(5),
+-- ID int(5),
 Wert char(15),
 InstanzID int (5),
-AttributtypID int (5));
+AttributtypID char(30),
+PRIMARY KEY(InstanzID, AttributtypID));
