edc96e40831b9c53025309582ce0ae7a65fb16d0
“Pascal <pascaljung6@freenet.de>
2020-09-28 12:32:12 +0200

    Datenbankupdate mit neuem TestDB Treiber

--- site/Datenbank/TestData.sql
+++ site/Datenbank/TestData.sql
@@ -12,39 +12,39 @@ INSERT INTO berechtigung (ID, Bezeichnung)
 VALUES (4, 'Teamleiter');
 
 -- User erstellen
-INSERT INTO User (ID, Name, Passwort, BerechtigungsID)
-VALUES (1, 'Peter', 'peter1234', 4);
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Peter', 'peter1234', 4);
 
-INSERT INTO User (ID, Name, Passwort, BerechtigungsID)
-VALUES (2, 'Max', 'max1234', 2);
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Max', 'max1234', 2);
 
-INSERT INTO User (ID, Name, Passwort, BerechtigungsID)
-VALUES (3, 'Daniel', 'daniel1234', 1);
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Daniel', 'daniel1234', 1);
 
-INSERT INTO User (ID, Name, Passwort, BerechtigungsID)
-VALUES (4, 'Dimitri', 'dimitri1234', 3);
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Dimitri', 'dimitri1234', 3);
 
-INSERT INTO User (ID, Name, Passwort, BerechtigungsID)
-VALUES (5, 'Johanna', 'johanna1234', 2);
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Johanna', 'johanna1234', 2);
 
 -- ConfigItemtyp erstellen
-INSERT INTO ConfigItemTyp (ID, Name)
-VALUES (1, 'Computer');
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Computer');
 
-INSERT INTO ConfigItemTyp (ID, Name)
-VALUES (2, 'Drucker');
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Drucker');
 
-INSERT INTO ConfigItemTyp (ID, Name)
-VALUES (3, 'Router');
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Router');
 
-INSERT INTO ConfigItemTyp (ID, Name)
-VALUES (4, 'Bildschirm');
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Bildschirm');
 
-INSERT INTO ConfigItemTyp (ID, Name)
-VALUES (5, 'Tastatur');
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Tastatur');
 
-INSERT INTO ConfigItemTyp (ID, Name)
-VALUES (6, 'Maus');
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Maus');
 
 --Instanzen erstellen
 INSERT INTO Instanz (ID, ConfigItemTypname, Name)
@@ -115,48 +115,60 @@ VALUES (21, 'Drucker', 'Samsung MultiXpress C9251NA CLX-9251');
 
 
 --Attributtypen erstellen
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (1, 'Farbe', 'Drucker');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Farbe', 'Drucker');
 
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (2, 'Blattdruckanzahl/m', 'Drucker');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Blattdruckanzahl/m', 'Drucker');
 
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (3, 'Speicher', 'Computer');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Speicher', 'Computer');
 
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (4, 'Arbeitsspeicher', 'Computer');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Arbeitsspeicher', 'Computer');
 
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (4, 'Prozessor', 'Computer');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Prozessor', 'Computer');
 
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (5, 'Groeße', 'Bildschirm');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Groeße', 'Bildschirm');
 
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (6, 'Auflösung', 'Bildschirm');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Auflösung', 'Bildschirm');
 
-INSERT INTO AttributTyp (ID, Name, ConfigItemTypname)
-VALUES (7, 'Reaktionszeit', 'Bildschirm');
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Reaktionszeit', 'Bildschirm');
 
 
 --Attribute erstellen
-INSERT INTO Attribut (ID, Wert, InstanzID, AttributtypID)
-VALUES (1, 'weiß', 20, 1);
+--Drucker1
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('weiß', 20, 'Farbe');
 
-INSERT INTO Attribut (ID, Wert, InstanzID, AttributtypID)
-VALUES (2, '25', 20, 2);
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('25', 20, 'Blattdruckanzahl/m');
 
-INSERT INTO Attribut (ID, Wert, InstanzID, AttributtypID)
-VALUES (3, '64MB', 20, 3);
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('64MB', 20, 'Speicher');
 
 
+--Drucker2
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('schwarz', 21, 'Farbe');
 
-INSERT INTO Attribut (ID, Wert, InstanzID, AttributtypID)
-VALUES (4, 'schwarz', 21, 1);
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('25', 21, 'Blattdruckanzahl/m');
 
-INSERT INTO Attribut (ID, Wert, InstanzID, AttributtypID)
-VALUES (5, '25', 21, 2);
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('64MB', 21, 'Speicher');
 
-INSERT INTO Attribut (ID, Wert, InstanzID, AttributtypID)
-VALUES (6, '64MB', 21, 3);
+
+--Computer
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('1000GB', 11, 'Speicher');
+
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('8GB', 11, 'Arbeitsspeicher');
+
+INSERT INTO Attribut (Wert, InstanzID, AttributtypID)
+VALUES ('3,2GHz 8Core', 11, 'Speicher');
