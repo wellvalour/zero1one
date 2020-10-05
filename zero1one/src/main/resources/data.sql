3901a66314f2bec8a643c32ad704f7893b4ed08f
“Til <wohlmuth.til@gmail.com>
2020-10-05 13:19:50 +0200

    Richtiges Schema zu Testen eingefügt

--- zero1one/src/main/resources/data.sql
+++ zero1one/src/main/resources/data.sql
@@ -1,32 +1,171 @@
 
+INSERT INTO berechtigung (ID, Bezeichnung)
+VALUES (1, 'Admin');
 
---
--- Daten f√ºr Tabelle `mitarbeiter`
---
+INSERT INTO berechtigung (ID, Bezeichnung)
+VALUES (2, 'Mitarbeiter');
 
-INSERT INTO `mitarbeiter` (`persnr`, `nachname`, `vorname`, `wohnort`, `beruf`, `gehalt`, `zulage`, `stellenr`, `gebdatum`, `einstelldatum`) VALUES
-(608, 'Schoene', 'Gernot', 'Essen', 'DV-Kaufmann', '3200.00', '1000.00', 10, '1976-09-23', '2004-01-01'),
-(939, 'Schmidt', 'Peter', 'Duisburg', 'Dipl. Informatiker', '2480.00', '0.00', 13, '1972-10-23', '2002-10-01'),
-(613, 'Gschwindle', 'Tamara', 'Neuss', 'Dipl. Ing.', '6420.00', '0.00', 2, '1971-02-02', '2009-08-01'),
-(121, 'Knaller', 'Ferdinand', 'Stuttgart', 'Mathematiker', '6650.00', '0.00', 3, '1963-03-13', '2008-02-01'),
-(641, 'Mayer', 'Fritz', 'Boeblingen', 'DV-Kaufmann', '3500.00', '0.00', 11, '1971-05-07', '2004-01-01'),
-(53, 'Sauter', 'Ernst', 'Stuttgart', 'DV-Kaufmann', '2275.00', '0.00', 6, '1968-01-05', '2006-08-01'),
-(391, 'Maier', 'Simone', 'Calw', 'Informatiker', '2930.00', '0.00', 12, '1979-08-07', '2002-11-01'),
-(396, 'Fauser', 'Frieda', 'Tuebingen', 'Betriebswirt', '6330.00', '0.00', 4, '1978-11-16', '2006-07-01'),
-(926, 'Kaspar', 'David', 'Nuertingen', 'Dipl. Informatiker', '2500.00', '0.00', 17, '1978-09-30', '2005-10-01'),
-(342, 'Marker', 'Annegret', 'Leonberg', 'DV-Kaufmann', '2466.00', '0.00', 14, '1973-12-23', '2002-09-01'),
-(181, 'Papsch', 'Brigitte', 'Ludwigsburg', 'Industrie-Kaufmann', '2100.00', '165.00', 21, '1985-06-09', '2009-08-01'),
-(949, 'Dannenmann', 'Sabine', 'Tuebingen', 'Bank-Kaufmann', '2200.00', '0.00', 8, '1975-08-21', '2005-05-01'),
-(936, 'Schmidt', 'Peter', 'Boeblingen', 'Techniker', '2095.00', '0.00', 16, '1982-09-22', '2005-09-01'),
-(345, 'Langner', 'Frowine', 'Ludwigsburg', 'Betriebswirt', '8500.00', '0.00', 1, '1963-03-12', '2009-06-01'),
-(277, 'Schuler', 'Vreni', 'Herrenberg', 'Technischer Kaufmann', '2800.00', '135.00', 19, '1986-08-05', '2010-05-01'),
-(74, 'Haller', 'Irene', 'Nagold', 'Dipl. Kaufmann', '2100.00', '0.00', 9, '1968-05-17', '2005-12-01'),
-(471, 'Raisch', 'Albert', 'Leonberg', 'Math.-Assistent', '1950.00', '180.00', 22, '1985-08-20', '2008-05-01'),
-(819, 'Vaihinger', 'Christa', 'Herrenberg', 'Kaufmann', '2205.00', '225.00', 5, '1971-04-12', '2001-02-01'),
-(283, 'Maier', 'Simone', 'Stuttgart', 'Dipl. Informatiker', '3385.00', '120.00', 24, '1975-01-08', '2002-08-01'),
-(465, 'Wagner', 'Gustav', 'Herrenberg', 'Vers.-Kaufmann', '3025.00', '210.00', 23, '1969-02-05', '2005-12-01'),
-(52, 'Nagel', 'Ludwig', 'Boeblingen', 'Techniker', '1995.00', '0.00', 7, '1956-07-12', '2006-10-01'),
-(356, 'Baessler', 'Erwin', 'Horb', 'Ingenieur', '2850.00', '120.00', 20, '1972-09-02', '2001-12-01'),
-(353, 'Zaengle', 'Toni', 'Reutlingen', 'Informatiker', '3150.00', '0.00', 15, '1961-02-09', '2002-12-01'),
-(439, 'Haberbosch', 'Klaus', 'Stuttgart', 'Betriebswirt', '3280.00', '0.00', 18, '1980-01-13', '2006-03-01');
+INSERT INTO berechtigung (ID, Bezeichnung)
+VALUES (3, 'Azubi');
 
+INSERT INTO berechtigung (ID, Bezeichnung)
+VALUES (4, 'Teamleiter');
+
+
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Peter', 'peter1234', 4);
+
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Max', 'max1234', 2);
+
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Daniel', 'daniel1234', 1);
+
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Dimitri', 'dimitri1234', 3);
+
+INSERT INTO User (Name, Passwort, BerechtigungsID)
+VALUES ('Johanna', 'johanna1234', 2);
+
+
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Computer');
+
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Drucker');
+
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Router');
+
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Bildschirm');
+
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Tastatur');
+
+INSERT INTO ConfigItemTyp (Name)
+VALUES ('Maus');
+
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (1, 'Maus', 'Logitech Maus');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (2, 'Maus', 'Logitech Maus');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (3, 'Maus', 'LogLink Maus');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (4, 'Maus', 'LogLink Maus');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (5, 'Tastatur', 'Logitech Tastatur');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (6, 'Tastatur', 'Logitech Tastatur');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (7, 'Tastatur', 'Logitech Tastatur');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (8, 'Tastatur', 'LogiLink Tastatur');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (9, 'Tastatur', 'LogiLink Tastatur');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (10, 'Router', 'FritzBox 7590');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (11, 'Computer', 'IMac Pro');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (12, 'Computer', 'ACER Predator Orion 9000');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (13, 'Computer', 'ACER Predator Orion 9000');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (14, 'Computer', 'ACER Predator Orion 9000');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (15, 'Computer', 'ACER Predator Orion 9000');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (16, 'Bildschirm', 'Lenovo L24-10');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (17, 'Bildschirm', 'Lenovo L24-10');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (18, 'Bildschirm', 'Samsung C24F396FH');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (19, 'Bildschirm', 'Samsung C24F396FH');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (20, 'Drucker', 'Samsung MultiXpress C9251NA CLX-9251');
+
+INSERT INTO Instanz (ID, ConfigItemTypname, Name)
+VALUES (21, 'Drucker', 'Samsung MultiXpress C9251NA CLX-9251');
+
+
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Farbe', 'Drucker');
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Blattdruckanzahl/m', 'Drucker');
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Speicher', 'Computer');
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Arbeitsspeicher', 'Computer');
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Prozessor', 'Computer');
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Groeﬂe', 'Bildschirm');
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Auflˆsung', 'Bildschirm');
+
+INSERT INTO AttributTyp (Name, ConfigItemTypname)
+VALUES ('Reaktionszeit', 'Bildschirm');
+
+
+
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (20, 'Farbe', 'weiﬂ');
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (20, 'Blattdruckanzahl/m', '25');
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (20, 'Speicher', '64MB');
+
+
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (21, 'Farbe', 'schwarz');
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (21, 'Blattdruckanzahl/m', '25');
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (21, 'Speicher', '64MB');
+
+
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (11, 'Speicher', '1000GB');
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (11, 'Arbeitsspeicher', '8GB');
+
+INSERT INTO Attribut (InstanzID, AttributtypID, Wert)
+VALUES (11, 'Prozessor', '3,2GHz 8Core');
