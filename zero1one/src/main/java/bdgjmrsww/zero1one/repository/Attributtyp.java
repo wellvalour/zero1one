09fcfb3d46ce2dfe156a5c609dc1648d54071ebb
johannagroell <groell.johannala@gmail.com>
2020-10-05 14:09:38 +0200

    Weitere Entitätsklassen hinzugefügt

--- zero1one/src/main/java/bdgjmrsww/zero1one/repository/Attributtyp.java
+++ zero1one/src/main/java/bdgjmrsww/zero1one/repository/Attributtyp.java
@@ -9,5 +9,22 @@ public class Attributtyp {
 	@Id
 	private String name;
 	
-	private String configitemTypname;
+	private String configItemTypname;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getConfigItemTypname() {
+		return configItemTypname;
+	}
+
+	public void setConfigItemTypname(String configItemTypname) {
+		this.configItemTypname = configItemTypname;
+	}
+	
 }
