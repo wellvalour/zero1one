436448de46c7c35061aed48f34e63db8465f5183
“Til <wohlmuth.til@gmail.com>
2020-10-19 17:44:05 +0200

    Bugfix

--- zero1one/src/main/java/com/gruppezwei/zero1one/repository/ConfigitemRepository.java
+++ zero1one/src/main/java/com/gruppezwei/zero1one/repository/ConfigitemRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConfigitemRepository extends JpaRepository<Configitem, Long> {
+public interface ConfigitemRepository extends JpaRepository<Configitem, Integer> {
 	
 	List<Configitem> findByName(String name);
 	
