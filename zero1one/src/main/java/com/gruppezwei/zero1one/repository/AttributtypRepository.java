ea63c7366910b52c834e6a0f9210f1b3ce867501
“Til <wohlmuth.til@gmail.com>
2020-10-19 17:53:08 +0200

    Repos auf aktuellsten Stand gebracht

--- zero1one/src/main/java/com/gruppezwei/zero1one/repository/AttributtypRepository.java
+++ zero1one/src/main/java/com/gruppezwei/zero1one/repository/AttributtypRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttributtypRepository extends JpaRepository<Attributtyp, Long> {
+public interface AttributtypRepository extends JpaRepository<Attributtyp, String> {
 	
 	List<Attributtyp> findByConfigitemtypname(String configitemtypname);
 	
--- zero1one/src/main/java/com/gruppezwei/zero1one/repository/ConfigitemtypRepository.java
+++ zero1one/src/main/java/com/gruppezwei/zero1one/repository/ConfigitemtypRepository.java
@@ -2,6 +2,6 @@ package com.gruppezwei.zero1one.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConfigitemtypRepository extends JpaRepository<Configitemtyp, Long> {
+public interface ConfigitemtypRepository extends JpaRepository<Configitemtyp, String> {
 
 }
